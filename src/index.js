3dc2018016411152e30fcae78c7c213b92e1d3aa
dhillonks <52294825+dhillonks@users.noreply.github.com>
2020-02-03 23:19:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ if(csvData) {
   function initMap(intersections){
     let toronto = L.map('mapid').setView([43.6532, -79.3832], 12.2);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
